a36490fee17f1a74a39cfe94f0b45ed0ddd047ea
loglogin2 <i9991559@gmail.com>
2017-03-06 19:02:43 +0300

    Product/feature: wrong overflow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    // your solution
+    var c ;
+    var ans = '';
+    var o = 0;
+    var ret = '' ;
+    var nols = '';
+
+    function reverse(str) {
+        var ret = '';
+        for ( var i = str.length - 1 ; i >= 0 ; i-- )
+            ret += str[i];
+        return ret;
+    }
+
+    function sum_long(s,f){
+        o = 0;
+        ans = '';
+        if ( f.length < s.length )
+        {
+            var tmp = f;
+            f = s;
+            s = tmp;
+        }
+        for ( var i = 0 ; i < s.length ; i ++ )
+        {
+            var g = ( +f[i] + +s[i] + o ) % 10 ;
+            o = Math.floor( ( +f[i] + +s[i] ) / 10 );
+            ans += g ;
+        }
+        if ( s.length == f.length )
+        {
+            if ( o > 0 )
+                ans += o;
+            o = 0;
+            return ans;
+        }
+        for ( var i = s.length  ; i < f.length ; i ++ )
+        {
+            var g = +f[i] + o;
+            o = Math.floor(( +f[i] + o ) / 10);
+            ans += g;
+        }
+        if ( o > 0 )
+            ans += o;
+        o = 0;
+        return ans
+    }
+
+    if ( first.length < second.length )
+    {
+        var tmp = first;
+        first = second;
+        second = tmp;
+    }
+    var a = reverse( first );
+    var b = reverse( second );
+    for ( var i = 0 ; i < b.length ; i ++ )
+    {
+        ans = '';
+        ans = nols;
+        nols += 0;
+        for ( var j = 0 ; j < a.length; j++ )
+        {
+            c =( ( +b[i] * +a[j] ) + o ) % 10;
+            o = Math.floor( ( +b[i] * +a[j] ) / 10 );
+            ans += c;
+        }
+        if( o > 0 )
+            ans += o;
+        o = 0;
+        if ( ret.length == 0 )
+            ret = ans;
+        else
+            ret = sum_long(ret,ans);
+    }
+    ans = reverse(ret);
+    return ans;
 }
+
+
